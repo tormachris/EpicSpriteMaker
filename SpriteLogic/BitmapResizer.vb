3e86a4a60bcd7087d9cdf0dbc5221c6dd64b608e
tormakris <tormakristof@tormakristof.eu>
2019-07-23 21:13:45 +0200

    resizer done

--- SpriteLogic/BitmapResizer.vb
+++ SpriteLogic/BitmapResizer.vb
@@ -9,18 +9,50 @@ Public Class BitmapResizer
         Me._listOfBitmaps = New List(Of System.Drawing.Bitmap)
     End Sub
 
-    Public Sub AddBitmap(bitmap As System.Drawing.Bitmap)
+    Public Sub AddBitmap(ByRef bitmap As System.Drawing.Bitmap)
         Me._listOfBitmaps.Append(bitmap)
     End Sub
 
-    Public Sub AddImage(image As System.Drawing.Image)
+    Public Sub AddImage(ByRef image As System.Drawing.Image)
         Dim bitmap As System.Drawing.Bitmap = New System.Drawing.Bitmap(image)
         'We want to store bitmaps because they are easier to work with.
         Me._listOfBitmaps.Append(bitmap)
     End Sub
+
+    Public Sub ClearBitmaps()
+        Me._listOfBitmaps.Clear()
+    End Sub
+
+    Public Sub RemoveBitmap(ByRef bitmap As System.Drawing.Bitmap)
+        If Me._listOfBitmaps.Contains(bitmap) Then
+            Me._listOfBitmaps.Remove(bitmap)
+        End If
+    End Sub
+
+    Public Sub RemoveBitmapAt(ByVal index As Integer)
+        If index > 0 AndAlso index <= Me._listOfBitmaps.Count() - 1 Then
+            Me._listOfBitmaps.RemoveAt(index)
+        Else
+            Throw New IndexOutOfRangeException("Range must be within the bounds of the list of images.")
+        End If
+    End Sub
 #End Region
 
-    Public Function ResizeImage(bmSource As Drawing.Bitmap, targetWidth As Int32, targetHeight As Int32) As System.Drawing.Bitmap
+#Region "Logic"
+    Public Sub ResizeAll(ByVal targetWidth As Int32, ByVal targetHeight As Int32)
+        Dim bitmap As System.Drawing.Bitmap
+        For Each bitmap In Me._listOfBitmaps
+            bitmap = BitmapResizer.ResizeImage(bitmap, targetWidth, targetHeight)
+        Next bitmap
+    End Sub
+
+    Public Overloads Shared Function ResizeImage(ByRef sourceImage As Drawing.Image, ByVal targetWidth As Int32, ByVal targetHeight As Int32) As Drawing.Bitmap
+        Dim bmSource As System.Drawing.Bitmap = New Drawing.Bitmap(sourceImage)
+
+        Return ResizeImage(bmSource, targetWidth, targetHeight)
+    End Function
+
+    Public Overloads Shared Function ResizeImage(ByRef bmSource As Drawing.Bitmap, ByVal targetWidth As Int32, ByVal targetHeight As Int32) As System.Drawing.Bitmap
         Dim bmDest As New System.Drawing.Bitmap(targetWidth, targetHeight, System.Drawing.Imaging.PixelFormat.Format32bppArgb)
 
         Dim sourceAspectRatio As Double = bmSource.Width / bmSource.Height
@@ -57,4 +89,5 @@ Public Class BitmapResizer
 
         Return bmDest
     End Function
+#End Region
 End Class
